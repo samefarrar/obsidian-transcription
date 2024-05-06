5cfe1c49019477b22b910f6b3dffec58f8186ae8
FazMataz <samefarrar@gmail.com>
2024-05-06 13:17:08 +0100

    feat: Initial transcription functionality

--- main.ts
+++ main.ts
@@ -1,134 +1,120 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { createClient, Deepgram, LiveTranscriptionEvents } from "@deepgram/sdk";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DeepGramPluginSettings {
+	apiKey: string;
+	summarize: boolean;
+	topic_detection: boolean;
+	intent_detection: boolean;
+	sentiment: boolean;
+	smart_format: boolean;
+	punctuation: boolean;
+	paragraphs: boolean;
+	utterances: boolean;
+	filler_words: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DeepGramPluginSettings = {
+	apiKey: '',
+	summarize: false,
+	topic_detection: false,
+	intent_detection: false,
+	sentiment: false,
+	smart_format: false,
+	punctuation: false,
+	paragraphs: false,
+	utterances: false,
+	filler_words: false
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DeepgramPlugin extends Plugin {
+	settings: DeepgramPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
+	  await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	  this.addCommand({
+		id: 'start-transcription',
+		name: 'Start Transcription',
+		callback: () => {
+		  this.startTranscription();
+		},
+	  });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+	  this.addSettingTab(new DeepgramSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	  this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	  await this.saveData(this.settings);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	async startTranscription() {
+	  const deepgramClient = createClient(this.settings.apiKey);
+	  const deepgram = deepgramClient.listen.live({
+		language: "en",
+		punctuate: true,
+		smart_format: true,
+		model: "nova-2",
+	  });
+
+	  deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
+		console.log("deepgram: connected");
+	  });
+
+	  deepgram.addListener(LiveTranscriptionEvents.Transcript, (data) => {
+		console.log("deepgram: transcript received");
+		// Insert the transcribed text into the active editor
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+		  const editor = activeView.editor;
+		  editor.replaceRange(data.channel.alternatives[0].transcript, editor.getCursor());
+		}
+	  });
+
+	  deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
+		console.error("deepgram: error received", error);
+	  });
+
+	  // Start recording audio from the microphone
+	  const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+	  const mediaRecorder = new MediaRecorder(mediaStream, { mimeType: "audio/webm" });
+	  mediaRecorder.addEventListener("dataavailable", (event) => {
+		if (event.data.size > 0) {
+		  deepgram.send(event.data);
+		}
+	  });
+	  mediaRecorder.start(1000);
 	}
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  class DeepgramSettingTab extends PluginSettingTab {
+	plugin: DeepgramPlugin;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	constructor(app: App, plugin: DeepgramPlugin) {
+	  super(app, plugin);
+	  this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+	  const { containerEl } = this;
 
-		containerEl.empty();
+	  containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	  new Setting(containerEl)
+		.setName('Deepgram API Key')
+		.setDesc('Enter your Deepgram API key')
+		.addText(text => text
+		  .setPlaceholder('API Key')
+		  .setValue(this.plugin.settings.apiKey)
+		  .onChange(async (value) => {
+			this.plugin.settings.apiKey = value;
+			await this.plugin.saveSettings();
+		  }));
 	}
-}
+  }
